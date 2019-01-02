bf9d17b8394630bf86e8a65984c6f682c8e7281c
soubhagya <soubhagya@recruitmaze.com>
2019-01-02 17:12:16 +0530

    adding postgres in production

--- djangodeploy/urls.py
+++ djangodeploy/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
+from .views import home
 
 urlpatterns = [
     path('', home),
